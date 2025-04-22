d0b7ab836d8325881c94518794a493dd13a614f0
Antonio Mario Pio <antonio.frioli1@gmail.com>
2025-04-22 17:18:27 -0300

    refact( mqtt & wifi max retries )

--- sensor_rain/mqtt_utils.cpp
+++ sensor_rain/mqtt_utils.cpp
@@ -10,8 +10,9 @@ void mqttSetup() {
     client.setServer(MQTT_BROKER_URL, MQTT_BROKER_PORT);
 }
 
-void mqttConnect(int connectionAttempsDelay = 2000) {
-    while (!client.connected()) {
+void mqttConnect(int connectionAttempsDelay = 2000, int Maxtries = 20) {
+    int tries = 0;
+    while (!client.connected() && tries < Maxtries) {
         Serial.print("Attempting to connect to MQTT broker...");
         if (client.connect(MQTT_BROKER_CLIENT_ID, MQTT_BROKER_USERNAME, MQTT_BROKER_PASSWORD)) {
             Serial.println("Connected to the broker!");
@@ -20,6 +21,7 @@ void mqttConnect(int connectionAttempsDelay = 2000) {
             Serial.println(client.state());
             delay(connectionAttempsDelay);  
         }
+        tries++;
     }
 }
 
--- sensor_rain/wifi_utils.cpp
+++ sensor_rain/wifi_utils.cpp
@@ -4,15 +4,9 @@
 void wifiConnect() {
   WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
 
-  int tries = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
-    tries++;
-    if (tries > 20) {
-      Serial.println("Failed to connect to Wi-Fi!");
-      return;
-    }
+    Serial.println("Wi-Fi not connected, retrying...");
   }
 
   Serial.println("\nConnected to Wi-Fi!");
