04dbd7e4a3e7f9b4510ca5f9f3f52ed65cffe95a
Antonio Mario Pio Frioli <antonio.frioli1@gmail.com>
2025-05-13 22:34:52 -0300

    feat( temperature )

--- sensor_rain/sensor_rain.ino
+++ sensor_rain/sensor_rain.ino
@@ -2,36 +2,38 @@
 #include "mqtt_utils.h"
 
 #define SENSOR_PIN 19
-#define CHECK_INTERVAL 1000      
-#define MESSAGE_INTERVAL 10000   
-
-void checkSensor(unsigned long now);
-bool isRaining();
+#define CHECK_INTERVAL 1000
+#define MESSAGE_INTERVAL 10000
+#define TEMPERATURE_THRESHOLD 50
 
 unsigned long lastCheck = 0;
 unsigned long lastSent = 0;
 
+void checkSensor(unsigned long now);
+bool isRaining();
+float readTemperature();
+void checkTemperature();
+
 void setup() {
-  pinMode(SENSOR_PIN, INPUT);  
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 
   wifiConnect();
-  
-  mqttSetup();  
+  mqttSetup();
   mqttConnect();
 }
 
 void loop() {
-  
   mqttLoop();
 
   if (!isWifiConnected()) {
-    Serial.println("Wi-Fi disconnected, reconnecting...");
+    Serial.println("Wi-Fi desconectado, reconectando...");
     wifiConnect();
   }
 
   if (!isMqttConnected()) {
-    Serial.println("MQTT disconnected, reconnecting...");
+    Serial.println("MQTT desconectado, reconectando...");
     mqttConnect();
   }
 
@@ -39,19 +41,39 @@ void loop() {
   
   if (now - lastCheck >= CHECK_INTERVAL) {
     lastCheck = now;
-    checkSensor(now);
+    checkRainSensor(now);
+    checkTemperatureSensor();
   }
+
 }
 
-void checkSensor(unsigned long now){
-
+void checkRainSensor(unsigned long now) {
   if (isRaining() && (now - lastSent > MESSAGE_INTERVAL)) {
     publishMessage("Rain detected!");
     lastSent = now;
   }
-
 }
 
 bool isRaining() {
   return digitalRead(SENSOR_PIN) == LOW;
 }
+
+void checkTemperature() {
+  float temperature = readTemperature();
+  
+  if (temperature > TEMPERATURE_THRESHOLD) {
+    Serial.println("[STATUS] - Too hot");
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+float checkTemperatureSensor() {
+  int raw = analogRead(ESP32_TEMPERATURE_SENSOR);
+  float voltage = raw * 3.3 / 4095.0;
+  float temperature = (voltage - 0.5) * 100;
+  return temperature;
+}
+
+
